c5f9675fe258fb1125dd9056a666a58ca54a489e
VladPetrenko <59024535+vladptr@users.noreply.github.com>
2025-11-16 20:18:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 import os
 from discord.ext import commands
+from keep_alive import keep_alive
 
 intents = discord.Intents.default()
+intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -13,5 +16,6 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f"Проверка успешна {round(bot.latency * 1000)}ms")
 
-bot.run(os.getenv("TOKEN"))
+keep_alive()
 
+bot.run(os.getenv("TOKEN"))
